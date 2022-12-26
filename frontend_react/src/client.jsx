230705ee57590678575342dd78eec361ea760627
iruojefaith <fumsy240@gmail.com>
2022-12-26 20:31:51 +0100

    added about

---
+++ frontend_react/src/client.jsx
@@ -0,0 +1,11 @@
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+
+export const client = sanityClient({
+
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
