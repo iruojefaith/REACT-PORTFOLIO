2339f1a3aca5ca28c79b446efa5dbfdeae05c34c
iruojefaith <fumsy240@gmail.com>
2022-12-27 16:03:12 +0100

    added footer

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,9 +1,13 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
-
+const project = "934cybra"
 
 export const client = sanityClient({
-
+projectId: 'project',
+  dataset: 'production',
+  apiVersion: '2022-02-01',
+  useCdn: true,
+  token: import.meta.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
