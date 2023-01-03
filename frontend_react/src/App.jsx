75a75b82025f0ccdf19e320994c1571457087b7d
iruojefaith <fumsy240@gmail.com>
2023-01-03 22:25:40 +0100

    added video

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components';
 import './App.scss';
-import ParticlesBackground from './particles/ParticlesBackground';
+import particlesVideo from './assets/Particles.mp4'
+
 
 function App() {
 
 
-  return <div className='app'>
-        <ParticlesBackground />
+  return<div className='app'>
+   <div className=' relative w-full h-full'>
+      <video
+      src={particlesVideo}
+      type="video/mp4"
+      loop
+      controls={false}
+      muted
+      autoPlay className='w-full h-full object-cover fixed '>
+      </video>
+      <div className='absolute top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+
     <Navbar />
       <Header />
       <About />
@@ -16,6 +27,8 @@ function App() {
       <Skills />
       <Testimonial />
   <Footer/>
+  </div>
+  </div>
 </div>
 }
 export default App;
